8729a18ae5b0d2ed73d8ee04a6744d9e6c76f3b5
tweenko <tweenko23@gmail.com>
2024-05-01 22:16:38 +0500

    5/1/2024

---
+++ nav.js
@@ -0,0 +1,6 @@
+document.write("<div class=\"navbar\">");
+document.write("    <a href=\"index.html\">GAME</a>");
+document.write("    <a href=\"status-updates.html\">STATUS UPDATES</a>");
+document.write("    <a href=\"https://soundcloud.com/dr-fansong-thing/sets/deltarune-chapter-3-7-fangame-ost\">SOUNDTRACK</a>");
+document.write("    <a href=\"team.html\">TEAM</a>");
+document.write("</div>");
